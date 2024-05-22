59c6be785813d688df5521b5b0996ee59830def2
Julian Camilo Benavides Vanegas <correoj94@gmail.com>
2024-05-21 21:00:23 -0500

    botón editar

--- FRONT-END/main.js
+++ FRONT-END/main.js
@@ -24,41 +24,49 @@ function leer() {
       console.log(respuesta.datos);
       const tabla = document.getElementById("tabla");
       respuesta.datos.forEach((producto) => {
-        tabla.innerHTML += `<tr> <td> ${
-          isNaN(producto.nombre) ? producto.nombre : "nombre inválido"
-        } </td> <td> ${
-          producto.precio ? producto.precio : "Producto sin precio"
-        }</td> </tr>`;
-      });
+        tabla.innerHTML += `<tr>
+                                <td> ${producto.nombre}</td>
+                                <td> ${ producto.precio }</td>
+                                <td>
+                                      <button type="button" onclick="editar()">Editar</button>    
+                                      <button type="button">Eliminar</button>    
+                                </td>
+                            </tr>`;
+        });
     })
     .catch((err) => alert(err));
 }
 
-function actualizar(idProducto, nombre, precio) {
-  console.log(`http://localhost:3000/api/producto/${idProducto}`);
-
-  fetch(`http://localhost:3000/api/producto/${idProducto}`, {
-    method: "put",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({nombre, precio})
-  })
-    .then((respuesta) => respuesta.json())
-    .then((respuesta) => {
-      /////////////////LÓGICA POSTERIOR A ACTUALIZAR DATO //////////////////////
-    alert("Producto Actualizado");
-    })
-    .catch((error) => alert("Ha ocurrido un error"));
+function editar(){
+  ////RELLENAR FORMULARIO//////////////
+  alert("RELLENAR formulario para editar")
 }
 
-function eliminar(idProducto){
-  fetch(`http://localhost:3000/api/producto/${idProducto}`, {
-    method: "delete",
-    headers: {"Content-Type": "application/json"}
-  })
-  .then(respuesta=>respuesta.json())
-  .then(respuesta=> {
-    //////LÓGICA POSTERIOR A UNA ELIMINACIÓN////////7
-    alert("Producto Eliminado");
-  })
-  .catch((error)=> alert("Ha ocurrido un error", error));
-}
+// function actualizar(idProducto, nombre, precio) {
+//   console.log(`http://localhost:3000/api/producto/${idProducto}`);
+
+//   fetch(`http://localhost:3000/api/producto/${idProducto}`, {
+//     method: "put",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({nombre, precio})
+//   })
+//     .then((respuesta) => respuesta.json())
+//     .then((respuesta) => {
+//       /////////////////LÓGICA POSTERIOR A ACTUALIZAR DATO //////////////////////
+//     alert("Producto Actualizado");
+//     })
+//     .catch((error) => alert("Ha ocurrido un error"));
+// }
+
+// function eliminar(idProducto){
+//   fetch(`http://localhost:3000/api/producto/${idProducto}`, {
+//     method: "delete",
+//     headers: {"Content-Type": "application/json"}
+//   })
+//   .then(respuesta=>respuesta.json())
+//   .then(respuesta=> {
+//     //////LÓGICA POSTERIOR A UNA ELIMINACIÓN////////7
+//     alert("Producto Eliminado");
+//   })
+//   .catch((error)=> alert("Ha ocurrido un error", error));
+// }
